7069ec04ca0565ae5e9a7b36a588d069f8d120e9
Charan9441 <charanakula9441@gmail.com>
2025-07-24 10:26:11 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,9 @@
+# Initialize sum to 0
+total_sum = 0
+
+# Iterate over numbers from 1 to 50 (inclusive)
+for number in range(1, 51):
+    total_sum += number
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
